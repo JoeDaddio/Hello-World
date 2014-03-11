42be55042b6c17438fe414c21f7425f798d32526
JoeDaddio <josephrbarrett@gmail.com>
2014-03-11 12:52:30 -0700

    load instructions

---
+++ resources.py
@@ -0,0 +1,18 @@
+import pyglet
+
+def center_image(image):
+    """sets an image's anchor point to its center"""
+    image.anchor_x = image.width/2
+    image.anchor_y = image.height/2
+
+pyglet.resource.path = ['../resources']
+pyglet.resource.reindex()
+
+player_image = pyglet.resource.image("player.png")
+center_image(player_image)
+
+bullet_image = pyglet.resource.image("bullet.png")
+center_image(bullet_image)
+
+asteroid image = pyglet.resource.image("asteroid.png")
+center_image(asteroid_image)
